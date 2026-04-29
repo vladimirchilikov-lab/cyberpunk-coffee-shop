8cf8c09001827c270f2a726ed9f612f30e4c1f6d
vladimirchilikov-lab <vladimir.chilikov@gmail.com>
2026-04-29 15:49:48 +0300

    Create CartContext.js

---
+++ context/CartContext.js
@@ -0,0 +1,25 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
+  const removeFromCart = (index) => {
+    setCart(cart.filter((_, i) => i !== index));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export const useCart = () => useContext(CartContext);
