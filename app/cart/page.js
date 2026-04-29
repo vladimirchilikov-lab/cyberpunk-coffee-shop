4a633d416260c6e91d79b797d12c51e92be9063e
vladimirchilikov-lab <vladimir.chilikov@gmail.com>
2026-04-29 15:47:36 +0300

    Create page.js

---
+++ app/cart/page.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Cart() {
+  return (
+    <div className="p-6 bg-black text-cyan-300 min-h-screen">
+      <h1>CART</h1>
+      <p>Checkout ще добавим след малко</p>
+    </div>
+  );
+}
